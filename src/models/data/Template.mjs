5bacb1460ad65ed1fc9d2be3c268e4b0587d2b07
Tryliom <alexishaldy@gmail.com>
2023-08-24 12:59:11 +0200

    Added name and description to templates

--- src/models/data/Template.mjs
+++ src/models/data/Template.mjs
@@ -2,16 +2,24 @@ import {ProjectRole} from "./ProjectRole.mjs";
 
 export class Template
 {
+    /** @type {string} */
+    Name = ""
+    /** @type {string} */
+    Description = ""
     /** @type {ProjectRole[]} */
     Roles = []
 
     constructor()
     {
+        this.Name = "";
+        this.Description = "";
         this.Roles = [];
     }
 
     FromJson(data)
     {
+        this.Name = data.Name;
+        this.Description = data.Description;
         this.Roles = [];
 
         for (const role of data.Roles)
